bae1872647a50da251bd95755ab5af7652279be3
caseylholston <caseylholston@gmail.com>
2017-02-19 20:15:08 -0500

    Set up initial bootstrap css, created style.css, laid out character selection area

---
+++ assets/javascript/game.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    
+    var firstNumber;
+    var secondNumber;
+    var result;
+
+    initial();
+    
+// This is the initializing functions that sets the beginning game state
+    function initial() {
+        $('#first-number').empty();
+        $('#operator').empty();
+        $('#second-number').empty();
+        $('#result').empty();
+
+        firstNumber = '';
+        secondNumber = '';
+        operator = ''
+        result = '';
+        first();
+    }
+// This function will make the characters stats
+        function makeCharacter(){
+        $('.number').on('click',function() {
+            firstNumber += this.value;
+            console.log(firstNumber);
+            $('#first-number').html(firstNumber);
+        });
+
+
+
+        $('.clear').on('click',function() {
+            initial();
+        });
+    }
+
+// This function will determine the outcome of the attack button
+        function makeCharacter(){
+        $('.number').on('click',function() {
+            firstNumber += this.value;
+            console.log(firstNumber);
+            $('#first-number').html(firstNumber);
+        });
+
+
+
+        $('.clear').on('click',function() {
+            initial();
+        });
+    }
+
+
+//  This is the function for the restart button
+    $('.clear').on('click',function() {
+            initial();
+        });
+
+
+};
